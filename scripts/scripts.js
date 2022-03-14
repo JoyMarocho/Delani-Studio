949be443ecc79d60adcc616762015c691129d257
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-03-14 15:46:29 +0300

    UPDATE ON FILES

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,59 +32,59 @@ $("#product").click(function () {
 });
 
 $(document).ready(function () {
-    $("#work1").mouseover(function () {
+    $("#image1").mouseover(function () {
         $("#overlay").show();
     }).mouseout(function () {
         $("#overlay").hide();
     });
 });
 $(document).ready(function () {
-    $("#work2").mouseover(function () {
-        $("#overlay").show();
+    $("#image2").mouseover(function () {
+        $("#overlay2").show();
     }).mouseout(function () {
         $("#overlay2").hide();
     });
 });
 $(document).ready(function () {
-    $("#work3").mouseover(function () {
-        $("#overlay").show();
+    $("#image3").mouseover(function () {
+        $("#overlay3").show();
     }).mouseout(function () {
         $("#overlay3").hide();
     });
 });
 $(document).ready(function () {
-    $("#work4").mouseover(function () {
-        $("#overlay").show();
+    $("#image4").mouseover(function () {
+        $("#overlay4").show();
     }).mouseout(function () {
         $("#overlay4").hide();
     });
 });
 
 $(document).ready(function () {
-    $("#work5").mouseover(function () {
-        $("#overlay").show();
+    $("#image5").mouseover(function () {
+        $("#overlay5").show();
     }).mouseout(function () {
         $("#overlay5").hide();
     });
-    $("#work6").mouseover(function () {
-        $("#overlay").show();
+    $("#image6").mouseover(function () {
+        $("#overlay6").show();
     }).mouseout(function () {
         $("#overlay6").hide();
     });
 });
-$("#work7").mouseover(function () {
-    $("#overlay").show();
+$("#image7").mouseover(function () {
+    $("#overlay7").show();
 }).mouseout(function () {
     $("#overlay7").hide();
 });
-$("#work8").mouseover(function () {
-    $("#overlay").show();
+$("#image8").mouseover(function () {
+    $("#overlay8").show();
 }).mouseout(function () {
     $("#overlay8").hide();
 });
 
 $(document).ready(function () {
-    $("form#form34A").submit(function (event) {
+    $("form#formFILL").submit(function (event) {
         event.preventDefault();
         var name = $("input#ENTER1").val();
         var email = $("input#ENTER0").val();
