ad34f5ed7ddc5ea0547fc44a8abfc77171d038e2
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-03-14 11:20:11 +0300

    update js

--- scripts/backtotop.js
+++ scripts/backtotop.js
@@ -1,10 +1,10 @@
-jQuery("#backtotop").click(function () {
+$("#backtotop").click(function () {
     jQuery("body,html").animate({
         scrollTop: 0
     }, 600);
 });
 
-jQuery(window).scroll(function () {
+$(window).scroll(function () {
     if (jQuery(window).scrollTop() > 150) {
         jQuery("#backtotop").addClass("visible");
     } else {
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    $("development-image").click(function () {
+    $("#development-image").click(function () {
         $("#development-image").slideDown('1500').hide('1000');
-        $("development").show('1500');
+        $("#development").show('1500');
     });
 });
 $("#development").click(function () {
-    $("#develpment").slideUp('1500');
+    $("#development").slideUp('1500');
     $("#development-image").slideDown('1500');
 });
 
 $(document).ready(function () {
-    $("design-image").click(function () {
+    $("#design-image").click(function () {
         $("#design-image").slideDown('1500').hide('1000');
-        $("design").show('1500');
+        $("#design").show('1500');
     });
 });
 $("#design").click(function () {
@@ -21,9 +21,9 @@ $("#design").click(function () {
 });
 
 $(document).ready(function () {
-    $("product-image").click(function () {
+    $("#product-image").click(function () {
         $("#product-image").slideDown('1500').hide('1000');
-        $("product").show('1500');
+        $("#product").show('1500');
     });
 });
 $("#product").click(function () {
